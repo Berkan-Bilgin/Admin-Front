39076c65eb0da8869bb9900628739e162754708a
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-10-23 22:30:52 +0300

    signup form added

--- src/components/SignUp/SignUpForm.tsx
+++ src/components/SignUp/SignUpForm.tsx
@@ -1,45 +1,130 @@
-import React, { useState } from 'react';
-import { Button, Form, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { AccountCircle, Lock, EmailRounded } from '@mui/icons-material';
+import { Button, Grid, TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Popup from '../Popup/Popup';
 
-function SignupForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    // Burada kayıt doğrulaması ve/veya API çağrısı yapabilirsiniz
-    console.log('Name:', name, 'Email:', email, 'Password:', password);
+
+const SignupForm: React.FC = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const navigate = useNavigate()
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(2, 'isim alanı çok kısa')
+      .required('Lütfen isminizi giriniz'),
+    email: Yup.string()
+      .email('Geçerli bir e-posta adresi giriniz.')
+      .required('E-posta alanı boş bırakılamaz.'),
+    password: Yup.string()
+      .min(8, 'Şifre en az 8 karakter olmalıdır.')
+      .required('Şifre alanı boş bırakılamaz.'),
+  });
+
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = async (values: any) => {
+    // Bu kısımda sunucuya kayıt isteğinizi gönderebilirsiniz
+    try {
+      const response = await axios.post('http://localhost:3001/api/register', values);
+      console.log('Response : ', response);
+
+      if (response.status === 201) {
+        console.log('Kayıt başarılı', values);
+
+        setIsPopupOpen(true);
+
+        // Kayıt olunduğunda kullanıcıyı login sayfasına yönlendirsin
+        setTimeout(() => {
+          setIsPopupOpen(false);
+          navigate('/login');
+        }, 2000);
+      } else {
+        // Sunucudan hata alındı
+        console.error('Kayıt hatası:', response.data.error);
+      }
+    } catch (error) {
+      console.error('İstek gönderme hatası:', error);
+    }
   };
 
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={6}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Adınız</Form.Label>
-              <Form.Control type="text" placeholder="Adınızı girin" value={name} onChange={(e) => setName(e.target.value)} required />
-            </Form.Group>
+    <Grid container justifyContent="center" alignItems="center" minHeight="90vh">
+      <Grid item xs={12} sm={6} lg={4}>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Field
+              as={TextField}
+              fullWidth
+              label="Name"
+              name="name"
+              variant="outlined"
+              style={{ marginTop: '20px' }}
+              InputProps={{
+                startAdornment: <AccountCircle />,
+              }}
+            />
+            <ErrorMessage name="name" component="div" />
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email adresi</Form.Label>
-              <Form.Control type="email" placeholder="Email girin" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </Form.Group>
+            <Field
+              as={TextField}
+              fullWidth
+              label="E-posta"
+              name="email"
+              variant="outlined"
+              style={{ marginTop: '20px' }}
+              InputProps={{
+                startAdornment: <EmailRounded />,
+              }}
+            />
+            <ErrorMessage name="email" component="div" />
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Şifre</Form.Label>
-              <Form.Control type="password" placeholder="Şifre" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            </Form.Group>
+            <Field
+              as={TextField}
+              fullWidth
+              type="password"
+              label="Şifre"
+              name="password"
+              variant="outlined"
+              style={{ marginTop: '20px' }}
+              InputProps={{
+                startAdornment: <Lock />,
+              }}
+            />
+            <ErrorMessage name="password" component="div" />
 
-            <Button variant="primary" type="submit">
-              Kaydol
+            <Button type="submit" variant="contained" color="primary" fullWidth
+              style={{ marginTop: '20px' }}>
+              Kayıt Ol
             </Button>
+            
+            {isPopupOpen && (
+            <Popup
+              message="Başarılı bir şekilde kayıt oldunuz."
+              onClose={() => {
+                setIsPopupOpen(false);
+                navigate('/login'); // Login sayfasına yönlendirin
+              }}
+            />
+          )}
+
           </Form>
-        </Col>
-      </Row>
-    </Container>
+        </Formik>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default SignupForm;
