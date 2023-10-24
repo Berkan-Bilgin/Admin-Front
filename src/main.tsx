9614b58d934e92e2e4b5fbaeb0f3d6d05a537ad6
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-10-25 00:33:24 +0300

    added ContextProviders

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { AuthContextProvider } from './context/AuthContext.tsx';
+import { EventContextProvider } from './context/EventContext.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <AuthContextProvider>
+    <EventContextProvider>
+      <App />
+    </EventContextProvider>
+  </AuthContextProvider>,
+);
