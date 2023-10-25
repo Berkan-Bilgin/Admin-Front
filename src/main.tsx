aa718df466cf41f4fec61df9abd32a4ba0715e4b
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-10-25 07:36:17 +0300

    added normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import 'normalize.css';
 import { AuthContextProvider } from './context/AuthContext.tsx';
 import { EventContextProvider } from './context/EventContext.tsx';
 
