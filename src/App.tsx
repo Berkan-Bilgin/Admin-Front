465781f82d7fb5f8c527cd82987edc3eab2ebe0d
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-10-26 12:01:23 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import { useAuthContext } from './hooks/useAuthContext';
+import CreateEvent from './pages/CreateEvent';
+import Events from './pages/Events';
+import Settings from './pages/Settings';
 
 const App = () => {
   const {
@@ -18,6 +21,9 @@ const App = () => {
             <Route path="/" element={user ? <Home /> : <Navigate to="/login" />}></Route>
             <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />}></Route>
             <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />}></Route>
+            <Route path="/create-event" element={user ? <CreateEvent /> : <Navigate to="/" />}></Route>
+            <Route path="/events" element={user ? <Events /> : <Navigate to="/" />}></Route>
+            <Route path="/settings" element={user ? <Settings /> : <Navigate to="/" />}></Route>
           </Routes>
         </Layout>
       </BrowserRouter>
