5677f09af814380c8b2a1d8ec3eaaf81f444325e
busracogul <cogulbusra@gmail.com>
2023-10-26 14:47:45 +0300

    login-signup-styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useLogin } from '../hooks/useLogin';
+import { Button, TextField, Typography, Container } from '@mui/material';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -8,21 +9,26 @@ const Login = () => {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-
     await login(email, password);
-
     console.log(email, password);
   };
+
   return (
-    <form className="Login" onSubmit={handleSubmit}>
-      <h3>Log in</h3>
-      <label htmlFor="">Email: </label>
-      <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
-      <label htmlFor="">Password: </label>
-      <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-      <button disabled={isLoading}>Log in</button>
-      {error && <div>{error}</div>}
-    </form>
+    <div className="login-form">
+      <Container maxWidth="xs">
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h5" className="login-form_title">
+            Log in
+          </Typography>
+          <TextField className="login-form_email" label="Email" variant="outlined" fullWidth value={email} onChange={(e) => setEmail(e.target.value)} />
+          <TextField className="login-form_password" type="password" label="Password" variant="outlined" fullWidth value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Button className="login-form_submit" variant="contained" color="primary" fullWidth disabled={isLoading} type="submit">
+            Log in
+          </Button>
+          {error && <div>{error}</div>}
+        </form>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,29 +1,34 @@
 import { useState } from 'react';
 import { useSignup } from '../hooks/useSignup';
+import { Button, Typography, Container, TextField } from '@mui/material';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
   const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-
     await signup(email, password);
-
     console.log(email, password);
   };
+
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign up</h3>
-      <label htmlFor="">Email: </label>
-      <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
-      <label htmlFor="">Password: </label>
-      <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-      <button disabled={isLoading}>Sign Up</button>
-      {error && <div>{error}</div>}
-    </form>
+    <div className="signup-form">
+      <Container maxWidth="xs">
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h5" className="signup-form_title">
+            Sign Up
+          </Typography>
+          <TextField className="signup-form_email" label="Email" variant="outlined" fullWidth onChange={(e) => setEmail(e.target.value)} value={email} />
+          <TextField className="signup-form_password" label="Password" variant="outlined" fullWidth onChange={(e) => setPassword(e.target.value)} value={password} />
+          <Button className="signup-form_submit" variant="contained" color="primary" fullWidth disabled={isLoading} type="submit">
+            Sign Up
+          </Button>
+          {error && <div>{error}</div>}
+        </form>
+      </Container>
+    </div>
   );
 };
 
