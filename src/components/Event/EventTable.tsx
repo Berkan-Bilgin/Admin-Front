f3e5805089360d45c6dbfbe99c40e5f1794526a3
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-10-30 08:36:28 +0300

    added mui data-grid component

--- src/components/Event/EventTable.tsx
+++ src/components/Event/EventTable.tsx
@@ -1,58 +1,77 @@
-import React, { useState } from 'react';
-import { Table, TableBody, TableCell, TableHead, TableRow, TextField, Pagination } from '@mui/material';
+import React from 'react';
+import { Box, IconButton } from '@mui/material';
+import { DataGrid, GridColDef, GridRenderCellParams, GridToolbar } from '@mui/x-data-grid';
+import { useEventContext } from '../../hooks/useEventContext';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import axios from 'axios';
 
 const EventTable = () => {
-  const [filter, setFilter] = useState('');
-  const [page, setPage] = useState(1);
-  const itemsPerPage = 5;
+  const { state, dispatch } = useEventContext();
+  const { events } = state;
 
-  const data = [
-    { id: 1, name: 'Ali' },
-    { id: 2, name: 'Ayşe' },
-    { id: 3, name: 'Mehmet' },
-    { id: 4, name: 'Fatma' },
-    { id: 5, name: 'Hasan' },
-    { id: 6, name: 'Hüseyin' },
-    { id: 7, name: 'Elif' },
-    { id: 8, name: 'Ömer' },
-    { id: 9, name: 'Zeynep' },
-    { id: 10, name: 'Yusuf' },
-    { id: 11, name: 'Emine' },
-    { id: 12, name: 'Ahmet' },
-    { id: 13, name: 'Selin' },
-    { id: 14, name: 'Berk' },
-    { id: 15, name: 'Sema' },
-    { id: 16, name: 'Murat' },
-    { id: 17, name: 'Nur' },
-    { id: 18, name: 'Kerem' },
-    { id: 19, name: 'Büşra' },
-    { id: 20, name: 'Emir' },
+  const handleEditClick = (id: string) => {
+    console.log('Editing:', id);
+  };
+
+  const handleDeleteClick = async (id: string) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/event/${id}`);
+
+      if (response.status === 200) {
+        dispatch({ type: 'DELETE_EVENT', payload: { _id: id } });
+        console.log(`Event with ID: ${id} deleted successfully.`);
+      } else {
+        console.error('Failed to delete event.');
+      }
+    } catch (error) {
+      console.error('Error while deleting event:', error);
+    }
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'index',
+      headerName: '#',
+      width: 70,
+      valueGetter: (params) => {
+        const index = events?.findIndex((row) => row._id === params.row._id);
+        return index !== undefined && index !== -1 ? index + 1 : 0;
+      },
+    },
+    { field: 'title', headerName: 'Title', width: 150 },
+    { field: 'category', headerName: 'Category', width: 150 },
+    { field: 'description', headerName: 'Description', width: 250 },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      width: 150,
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          <IconButton aria-label="edit" color="primary" onClick={() => handleEditClick(params.row._id)}>
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="delete" color="error" onClick={() => handleDeleteClick(params.row._id)}>
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
   ];
 
-  const filteredData = data.filter((item) => item.name.toLowerCase().includes(filter.toLowerCase()));
-  const paginatedData = filteredData.slice((page - 1) * itemsPerPage, page * itemsPerPage);
-
   return (
-    <div style={{ padding: 40 }}>
-      <TextField label="Filtrele" value={filter} onChange={(e) => setFilter(e.target.value)} />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>İsim</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {paginatedData.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.id}</TableCell>
-              <TableCell>{item.name}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Pagination count={Math.ceil(filteredData.length / itemsPerPage)} page={page} onChange={(_, value) => setPage(value)} />
-    </div>
+    <Box>
+      <Box mx="20px" mt="40px">
+        <DataGrid
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          rows={events || []}
+          columns={columns}
+          getRowId={(row) => row._id}
+        />
+      </Box>
+    </Box>
   );
 };
 
