9fb6889c738a16b08f3a7fabf059c408b9e87745
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-01 07:00:13 +0300

    fixed incorrect path name

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -3,7 +3,7 @@ import { IEvent } from '../interfaces/event';
 
 const BASE_URL = 'http://localhost:3000/api';
 
-export const fetchEvents = () => axios.get(`${BASE_URL}/events`).then((res) => res.data);
+export const fetchEvents = () => axios.get(`${BASE_URL}/event`).then((res) => res.data);
 export const createEvent = (eventData: IEvent) => axios.post(`${BASE_URL}/event`, eventData).then((res) => res.data);
 export const deleteEvent = (id: string) => axios.delete(`${BASE_URL}/event/${id}`).then((res) => res.data);
 export const updateEvent = (id: string, eventData: IEvent) => axios.put(`${BASE_URL}/event/${id}`, eventData).then((res) => res.data);
