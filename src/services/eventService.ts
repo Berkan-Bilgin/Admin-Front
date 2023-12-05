a0c073645db58f0eb51a69430d5a8a022f2ba021
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-12-05 22:23:30 +0300

    refactor: change base URL

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,9 +1,49 @@
 import axios from 'axios';
 import { IEvent } from '../interfaces/event';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://plankton-app-ouh3i.ondigitalocean.app/api';
 
-export const fetchEvents = () => axios.get(`${BASE_URL}/event`).then((res) => res.data);
-export const createEvent = (eventData: IEvent) => axios.post(`${BASE_URL}/event`, eventData).then((res) => res.data);
-export const deleteEvent = (id: string) => axios.delete(`${BASE_URL}/event/${id}`).then((res) => res.data);
-export const updateEvent = (id: string, eventData: IEvent) => axios.put(`${BASE_URL}/event/${id}`, eventData).then((res) => res.data);
+const apiClient = axios.create({ baseURL: BASE_URL });
+
+export const fetchEvents = async (headers = {}) => {
+  try {
+    const response = await apiClient.get('/event', { headers });
+    return response.data; // Veriyi geri döndür
+  } catch (error: any) {
+    console.error('Fetching events failed: ', error);
+    if (error.response) {
+      console.error('Server Error: ', error.response.data);
+    }
+    throw error; // Hata durumunda hata fırlat
+  }
+};
+
+export const createEvent = async (eventData: IEvent) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/event`, eventData);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating event:', error);
+    throw error; // Hata, üst bileşene devredilir
+  }
+};
+
+export const deleteEvent = async (id: string) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/event/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting event:', error);
+    throw error; // Hata, üst bileşene devredilir
+  }
+};
+
+export const updateEvent = async (id: string, eventData: IEvent) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/event/${id}`, eventData);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating event:', error);
+    throw error; // Hata, üst bileşene devredilir
+  }
+};
